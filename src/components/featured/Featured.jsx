7fa3ca89fcc103541fd6f589ab6f562ba957406f
Palash Mandal <pmpalashmandal@hmail.com>
2022-08-09 15:23:06 +0600

    feat: added progress bar component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,7 +1,53 @@
 import './featured.scss';
+import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+import ExpandLessOutlinedIcon from '@mui/icons-material/ExpandLessOutlined';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Featured = () => {
-  return <div className="featured">Featured</div>;
+  return (
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVertOutlinedIcon fontSize="small" />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={'70%'} strokeWidth={5} />
+        </div>
+        <p className="title">Total sales made today</p>
+        <p className="amount">$420</p>
+        <p className="desc">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad,
+          aspernatur.
+        </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult positive">
+              <ExpandMoreOutlinedIcon fontSize="small" />
+              <div className="resultAmount">$12.2K</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Week</div>
+            <div className="itemResult negative">
+              <ExpandLessOutlinedIcon fontSize="small" />
+              <div className="resultAmount">$12.2K</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult positive">
+              <ExpandMoreOutlinedIcon fontSize="small" />
+              <div className="resultAmount">$12.2K</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
